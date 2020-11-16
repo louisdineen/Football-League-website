991136272ef8f9239bdce2209c2cb7c4da781f95
Pepin-Dineen, Louis (UG - Computer Science) <lp00676@surrey.ac.uk>
2020-11-16 21:20:37 +0000

    3rd commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :results
-  get 'result/Result'
-  get 'welcome/fixtures'
-  get 'welcome/tables'
-  get 'welcome/contact'
   get 'welcome/index'
+  get 'table/index'
+  get 'results/index'
+  post 'welcome/contact'
+  post 'table/index'
+  post 'results/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
